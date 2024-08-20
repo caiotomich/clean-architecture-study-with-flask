eb35a048e6da535c0a782ed0d62927f40f79d346
Caio Tomich <caiotomich@gmail.com>
2024-08-19 22:50:38 -0300

    feat(api): Update API infrastructure and add new controller and tests

--- app/infrastructure/api/api.py
+++ app/infrastructure/api/api.py
@@ -9,6 +9,9 @@ def create_app(Config):
     db.init_app(app)
 
     # Import and register blueprints or routes
+    from app.infrastructure.api.controllers.api import api_controller
+    app.register_blueprint(api_controller)
+
     from app.infrastructure.api.controllers.users import user_controller
     app.register_blueprint(user_controller)
 
---
+++ app/tests/test_api.py
@@ -0,0 +1,27 @@
+from app.infrastructure.api.api import create_app
+from app.config.test import TestConfig
+import unittest
+import json
+
+class ApiTestCase(unittest.TestCase):
+    def setUp(self):
+        """Set up test environment"""
+        self.app = create_app(TestConfig)
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        """Clean up after tests"""
+        self.app_context.pop()
+
+    def test_root_endpoint(self):
+        """Test root endpoint"""
+        response = self.client.get('/')
+        obj = json.loads(str(response.data.decode('utf-8')))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(obj['success'], True)
+
+if __name__ == '__main__':
+    unittest.main()
